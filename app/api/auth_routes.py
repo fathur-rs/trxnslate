ec975110b8ec7314c2d7e461e0f6ede1e086f59d
fathur-rs <fathurrahmansyarief@gmail.com>
2024-08-02 18:41:35 -0700

    edit model_routes

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,4 +1,3 @@
-import logging
 from flask import request, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token, create_refresh_token
 from datetime import datetime
